9c854ab567d7bc0d4f95b64690d99796a065878d
alanssoares <alan.ssoares@live.com>
2016-04-12 14:36:41 -0300

    Adicionando nova classe fileuitl

---
+++ SPLMessage/src/br/com/message/util/FileUtil.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.message.util;
+
+import java.io.File;
+
+/**
+ * @author alsoares
+ *
+ */
+public class FileUtil {
+
+	public void uploadFile(File file) {
+		//Upload file not implemented
+	}
+}
