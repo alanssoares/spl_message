e7619df8cb2509aa9b39c1c45989f11bb7851182
alanssoares <alan.ssoares@live.com>
2016-04-13 12:09:54 -0300

    Atualizando envio de email

--- SPLMessage/src/br/com/message/util/SendEmail.java
+++ SPLMessage/src/br/com/message/util/SendEmail.java
@@ -3,10 +3,15 @@
  */
 package br.com.message.util;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Date;
 import java.util.Properties;
 
+import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -17,23 +22,29 @@ import javax.mail.internet.MimeMessage;
  *
  */
 public class SendEmail {
-
-	private static final String HOST_DEFAULT = "localhost";
-	private static final String HOST_SMTP = "mail.smtp.host";
-	private static final String MAIL_USER = "mail.user";
-	private static final String MAIL_PASSWORD = "mail.password";
-	private static final String EMAIL_DEFAULT = "alansansoa@gmail.com";
+	
+	private static final String CONFIG_MAIL = "resources/mail.properties";
+	private static final String USERNAME = "mail.user";
+	private static final String PASSWORD = "mail.password";
+	
 	// Get system properties
 	private Properties properties;
 	// Get the default Session object.
 	private Session session;
 
 	public SendEmail() {
-		this.properties = System.getProperties();
-		this.properties.setProperty(HOST_SMTP, HOST_DEFAULT);
-		this.properties.setProperty(MAIL_USER, "myuser");
-		this.properties.setProperty(MAIL_PASSWORD, "mypwd");
-		this.session = Session.getDefaultInstance(this.properties);
+		InputStream input = null;
+		this.properties = new Properties();
+		try {
+			input = new FileInputStream(CONFIG_MAIL);
+			this.properties.load(input);
+			this.session = Session.getDefaultInstance(this.properties, new Authenticator(){
+				protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(properties.getProperty(USERNAME), properties.getProperty(PASSWORD));
+            }});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public boolean send(Email email) {
@@ -41,21 +52,19 @@ public class SendEmail {
 			// Create a default MimeMessage object.
 			MimeMessage message = new MimeMessage(session);
 			// Set From: header field of the header.
-			if(email.getFrom() == null){
-				message.setFrom(new InternetAddress(EMAIL_DEFAULT));
-			} else {
-				message.setFrom(new InternetAddress(email.getFrom()));
-			}
+			message.setFrom(new InternetAddress(properties.getProperty(USERNAME)));
 			// Set To: header field of the header.
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(email.getTo()));
 			// Set Subject: header field
 			message.setSubject(email.getSubject());
 			// Now set the actual message
 			message.setText(email.getText());
+			// Today
+			message.setSentDate(new Date());
 			// Send message
 			Transport.send(message);
 		} catch (MessagingException e) {
-			return false;
+			e.printStackTrace();
 		}
 
 		return true;
